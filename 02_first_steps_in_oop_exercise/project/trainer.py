a3641de1cb0639018ca29508f3f755ab37353175
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2022-10-20 21:00:31 +0300

    finished

--- 02_first_steps_in_oop_exercise/project/trainer.py
+++ 02_first_steps_in_oop_exercise/project/trainer.py
@@ -1,4 +1,4 @@
-from pokemon import Pokemon
+from project.pokemon import Pokemon
 
 
 class Trainer:
@@ -8,17 +8,16 @@ class Trainer:
 
     def add_pokemon(self, pokemon):
         pokemons_list = [x.name for x in self.pokemons]
-        print(pokemons_list)
-        if pokemon not in pokemons_list:
+        if pokemon.name in pokemons_list:
+            return f'This pokemon is already caught'
+        else:
             self.pokemons.append(pokemon)
             return f'Caught {pokemon.pokemon_details()}'
-        else:
-            return f'This pokemon is already caught'
 
     def release_pokemon(self, pokemon_name: str):
-        for i, name in enumerate(self.pokemons):
-            if pokemon_name == name.name:
-                del self.pokemons[i]
+        for i, x in enumerate(self.pokemons):
+            if x.name == pokemon_name:
+                self.pokemons.pop(i)
                 return f'You have released {pokemon_name}'
         return f'Pokemon is not caught'
 
@@ -27,19 +26,19 @@ class Trainer:
         return_string += f'Pokemon Trainer {self.name}\n'
         return_string += f'Pokemon count {len(self.pokemons)}\n'
         for data in self.pokemons:
-            return_string += f'{data.pokemon_details()}\n'
+            return_string += f'- {data.pokemon_details()}\n'
         return return_string
 
 
-pokemon = Pokemon("Pikachu", 90)
-print(pokemon.pokemon_details())
-trainer = Trainer("Ash")
-print(trainer.add_pokemon(pokemon))
-second_pokemon = Pokemon("Charizard", 110)
-print(trainer.add_pokemon(second_pokemon))
-print(trainer.add_pokemon(second_pokemon))
-print(trainer.release_pokemon("Pikachu"))
-print(trainer.release_pokemon("Pikachu"))
-print(trainer.trainer_data())
+# pokemon = Pokemon("Pikachu", 90)
+# print(pokemon.pokemon_details())
+# trainer = Trainer("Ash")
+# print(trainer.add_pokemon(pokemon))
+# second_pokemon = Pokemon("Charizard", 110)
+# print(trainer.add_pokemon(second_pokemon))
+# print(trainer.add_pokemon(second_pokemon))
+# print(trainer.release_pokemon("Pikachu"))
+# print(trainer.release_pokemon("Pikachu"))
+# print(trainer.trainer_data())
 
 # TODO: not ready
