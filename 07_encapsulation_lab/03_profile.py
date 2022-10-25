fc50a607437144e5fa861de783c10ddbe85ddd54
Velin Iliev <veliniliev@velins-mbp.client.bg.r4.madm.net>
2022-10-25 14:07:01 +0300

    Encapsulation - Lab

---
+++ 07_encapsulation_lab/03_profile.py
@@ -0,0 +1,26 @@
+class Profile:
+    def __init__(self, username: str, password: str):
+        if len(username) < 5 or len(username) > 15:
+            raise ValueError(f'The username must be between 5 and 15 characters.')
+        count_upper_case = 0
+        count_digits = 0
+        for char in password:
+            if char.isupper():
+                count_upper_case += 1
+            elif char.isdigit():
+                count_digits += 1
+        if len(password) < 8 or count_upper_case < 1 or count_digits < 1:
+            raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+        self.__username = username
+        self.__password = password
+
+    def __str__(self):
+        return f'You have a profile with username: "{self.__username}" and password: {"*" * len(self.__password)}'
+
+
+# profile_with_invalid_password = Profile('My_username', 'My-password')
+# #
+# profile_with_invalid_username = Profile('Too_long_username', 'Any')
+
+correct_profile = Profile("Username", "Passw0rd")
+print(correct_profile)
---
+++ 07_encapsulation_lab/04_email_validator.py
@@ -0,0 +1,42 @@
+class EmailValidator:
+    def __init__(self, min_length:int, mails: list, domains: list):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, name):
+        if len(name) < self.min_length:
+            return False
+        else:
+            return True
+
+    def __is_mail_valid(self, mail):
+        if mail in self.mails:
+            return True
+        else:
+            return False
+
+    def __is_domain_valid(self, domain):
+        if domain in self.domains:
+            return True
+        else:
+            return False
+
+    def validate(self, email):
+        x = email.split("@")
+        username = x[0]
+        mail, domain = x[1].split(".")
+        if self.__is_name_valid(username) and self.__is_mail_valid(mail) and self.__is_domain_valid(domain):
+            return True
+        else:
+            return False
+
+
+
+mails = ["gmail", "softuni"]
+domains = ["com", "bg"]
+email_validator = EmailValidator(6, mails, domains)
+print(email_validator.validate("pe77er@gmail.com"))
+print(email_validator.validate("georgios@gmail.net"))
+print(email_validator.validate("stamatito@abv.net"))
+print(email_validator.validate("abv@softuni.bg"))
