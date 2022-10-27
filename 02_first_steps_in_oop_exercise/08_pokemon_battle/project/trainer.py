77a8db0ef8ab891bef7781baa684bffa179189e2
Velin Iliev <veliniliev@velins-mbp.client.bg.r4.madm.net>
2022-10-27 15:21:37 +0300

    ref

--- 02_first_steps_in_oop_exercise/08_pokemon_battle/project/trainer.py
+++ 02_first_steps_in_oop_exercise/08_pokemon_battle/project/trainer.py
@@ -30,14 +30,3 @@ class Trainer:
         return return_string
 
 
-# pokemon = Pokemon("Pikachu", 90)
-# print(pokemon.pokemon_details())
-# trainer = Trainer("Ash")
-# print(trainer.add_pokemon(pokemon))
-# second_pokemon = Pokemon("Charizard", 110)
-# print(trainer.add_pokemon(second_pokemon))
-# print(trainer.add_pokemon(second_pokemon))
-# print(trainer.release_pokemon("Pikachu"))
-# print(trainer.release_pokemon("Pikachu"))
-# print(trainer.trainer_data())
-
