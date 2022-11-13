0c5e6abfa1007e932ca9b69e198cb0ceddee6c3b
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2022-11-13 13:17:40 +0200

    Polymorphism and Abstraction - Exercise

--- 12_polymorphism_and_abstraction_exercise/05_formula1_manager/project/f1_season_app.py
+++ 12_polymorphism_and_abstraction_exercise/05_formula1_manager/project/f1_season_app.py
@@ -1,10 +1,10 @@
-from project.formula_teams.red_bull_team import RedBullTeam
 from project.formula_teams.mercedes_team import MercedesTeam
+from project.formula_teams.red_bull_team import RedBullTeam
 
 
-class F1SeasonApp:
-    mercedes_team = None
+class F1SeasonApp():
     red_bull_team = None
+    mercedes_team = None
 
     def __init__(self):
         pass
@@ -21,7 +21,7 @@ class F1SeasonApp:
 
     def new_race_results(self, race_name: str, red_bull_pos: int, mercedes_pos: int):
         if self.mercedes_team is None or self.red_bull_team is None:
-            raise Exception('Not all teams have registered for the season.')
+            raise Exception(f'Not all teams have registered for the season.')
         else:
             return_sting = ""
             return_sting += f'Red Bull: {self.red_bull_team.calculate_revenue_after_race(red_bull_pos)}. '
@@ -33,3 +33,12 @@ class F1SeasonApp:
                 winner = "Mercedes"
             return_sting += f'{winner} is ahead at the {race_name} race.'
             return return_sting
+
+#  new_race_results(race_name: str, red_bull_pos: int, mercedes_pos: int)
+# •	If Red Bull or Mercedes haven't registered yet, raise an Exception with the
+# following message: "Not all teams have registered for the season."
+# •	Otherwise, find which team has the better position in the race, calculate
+# every team's revenue, update their budget, and return the following message:
+# "Red bull: { Red Bull revenue message }. Mercedes: { Mercedes revenue message }.
+# { team with better position } is ahead at the { race name } race."
+# •	Note: Teams' positions will always be valid.
--- 12_polymorphism_and_abstraction_exercise/05_formula1_manager/project/formula_teams/mercedes_team.py
+++ 12_polymorphism_and_abstraction_exercise/05_formula1_manager/project/formula_teams/mercedes_team.py
@@ -1,21 +1,18 @@
 from project.formula_teams.formula_team import FormulaTeam
 
-
 class MercedesTeam(FormulaTeam):
-    def __init__(self, budget: int):
-        super().__init__(budget)
+    expenses = 200000
 
     def calculate_revenue_after_race(self, race_pos: int):
-        expenses = 200000
-        sponsors = 0
-        if race_pos == 1:
-            sponsors += 1000000
+        earned_money = 0
+        if race_pos <= 1:
+            earned_money += 1000000
         elif race_pos <= 3:
-            sponsors += 500000
+            earned_money += 500000
         if race_pos <= 5:
-            sponsors += 100000
+            earned_money += 100000
         elif race_pos <= 7:
-            sponsors += 50000
-        revenue = sponsors - expenses
+            earned_money += 50000
+        revenue = earned_money - self.expenses
         self.budget += revenue
         return f'The revenue after the race is {revenue}$. Current budget {self.budget}$'
