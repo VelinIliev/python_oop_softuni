ddc19ddc9a23d2f1055deeadf20caf47d93139e1
Velin Iliev <73158924+VelinIliev@users.noreply.github.com>
2023-03-10 23:06:17 +0200

    refactor

--- 10_static_and_class_methods_exercise/01_photo_album.py
+++ 10_static_and_class_methods_exercise/01_photo_album.py
@@ -2,27 +2,26 @@ from math import ceil
 
 
 class PhotoAlbum():
+    PAGE_LENGTH = 4
+
     def __init__(self, pages: int):
         self.pages = pages
         self.photos = [[] for _ in range(pages)]
-        self.page_length = 4
 
     @classmethod
     def from_photos_count(cls, photos_count: int):
-        pages = ceil(photos_count / 4)
+        pages = ceil(photos_count / PhotoAlbum.PAGE_LENGTH)
         return cls(pages)
 
     def add_photo(self, label: str):
         for i, page in enumerate(self.photos, 1):
-            if len(page) >= self.page_length:
-                continue
-            else:
+            if len(page) < PhotoAlbum.PAGE_LENGTH:
                 page.append(label)
                 return f'{label} photo added successfully on page {i} slot {len(self.photos[i-1])}'
         return f'No more free slots'
 
     def display(self):
-        output = [f'{"-"*11}']
+        output = [f'{"-" * 11}']
         for page in self.photos:
             output.append(f'{" ".join("[]" for _ in range(len(page)))}')
             output.append(f'{"-"*11}')
--- 10_static_and_class_methods_exercise/02_movie_world/project/movie_world.py
+++ 10_static_and_class_methods_exercise/02_movie_world/project/movie_world.py
@@ -17,33 +17,40 @@ class MovieWorld:
         return 10
 
     def add_customer(self, customer: Customer):
-        if len(self.customers) < MovieWorld.customer_capacity():
+        if len(self.customers) < self.customer_capacity():
             self.customers.append(customer)
 
     def add_dvd(self, dvd: DVD):
-        if len(self.dvds) < MovieWorld.dvd_capacity():
+        if len(self.dvds) < self.dvd_capacity():
             self.dvds.append(dvd)
 
     def rent_dvd(self, customer_id: int, dvd_id: int):
         current_customer = next(filter(lambda x: x.id == customer_id, self.customers), None)
         current_dvd = next(filter(lambda x: x.id == dvd_id, self.dvds), None)
+
         if current_dvd in current_customer.rented_dvds:
             return f'{current_customer.name} has already rented {current_dvd.name}'
+
         if current_dvd.is_rented:
             return f'DVD is already rented'
+
         if current_customer.age < current_dvd.age_restriction:
             return f'{current_customer.name} should be at least {current_dvd.age_restriction} to rent this movie'
+
         current_customer.rented_dvds.append(current_dvd)
         current_dvd.is_rented = True
+
         return f'{current_customer.name} has successfully rented {current_dvd.name}'
 
     def return_dvd(self, customer_id, dvd_id):
         current_customer = next(filter(lambda x: x.id == customer_id, self.customers), None)
         current_dvd = next(filter(lambda x: x.id == dvd_id, self.dvds), None)
+
         if current_customer and current_dvd in current_customer.rented_dvds:
             current_customer.rented_dvds.remove(current_dvd)
             current_dvd.is_rented = False
             return f"{current_customer.name} has successfully returned {current_dvd.name}"
+
         return f'{current_customer.name} does not have that DVD'
 
     def __repr__(self):
